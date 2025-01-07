158219e98b72bebbdb2dc89386c16d8f05bba482
harshi1298 <11082004harshit@gmail.com>
2025-01-07 14:01:13 +0530

    database connected

--- server/server.js
+++ server/server.js
@@ -1,4 +1,9 @@
 import app from './app.js';
+// to use the environment variable
+import dotenv from 'dotenv';
+dotenv.config({ path: './variable.env' });
+
+import mongoose from 'mongoose';
 
 //register the route
 app.get('/', (req, res) => {
@@ -6,7 +11,17 @@ app.get('/', (req, res) => {
 });
 
 //server start listening
-const port = 3000;
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Server is created succesfully and run on the port ${port}`);
 });
+
+//connect the database
+mongoose
+  .connect(process.env.CONN_STR)
+  .then(() => {
+    console.log('Created database succesfully');
+  })
+  .catch((error) => {
+    console.log(`DB Cconnection failed due to ${error}`);
+  });
