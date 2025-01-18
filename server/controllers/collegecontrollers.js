3b117c3a897f2773466bf0404177379cc2c57bd5
harshi1298 <11082004harshit@gmail.com>
2025-01-18 11:59:53 +0530

    collegeroute updated

---
+++ server/controllers/collegecontrollers.js
@@ -0,0 +1,7 @@
+import asynchandler from '../utils/asynchandler.js';
+
+export const addcollege = asynchandler(async (req, res, next) => {});
+
+export const delcollege = asynchandler(async (req, res, next) => {});
+
+export const updatecollege = asynchandler(async (req, res, next) => {});
---
+++ server/routers/collegeroutes.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+import {
+  addcollege,
+  delcollege,
+  updatecollege,
+} from '../controllers/collegecontrollers.js';
+
+const router = express.Router();
+//create
+router.post('/create', addcollege);
+//delete
+router.delete('/del', delcollege);
+//update
+router.patch('/update', updatecollege);
