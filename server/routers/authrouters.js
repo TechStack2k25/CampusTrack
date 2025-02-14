93ea2035686502d745a398d605be4b7e18dfbb83
harshi1298 <11082004harshit@gmail.com>
2025-02-14 22:50:06 +0530

    forgot,reset,update password controller updated

--- server/routers/authrouters.js
+++ server/routers/authrouters.js
@@ -4,11 +4,14 @@ import {
   login,
   forgotpassword,
   logout,
+  resetpassword,
+  updatepassword,
 } from '../controllers/authcontrollers.js';
 
 const router = express.Router();
 router.post('/signup', signup);
 router.post('/login', login);
 router.post('/forgotpassword', forgotpassword);
+router.post('/resetpassword', resetpassword);
 router.get('/logout', logout);
 export default router;
