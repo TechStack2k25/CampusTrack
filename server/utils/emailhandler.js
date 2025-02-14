b502c9c3c8ccb1d153156f42bc083a606ca833a4
harshi1298 <11082004harshit@gmail.com>
2025-02-15 00:20:51 +0530

    use sendgrid to send mail

--- server/utils/emailhandler.js
+++ server/utils/emailhandler.js
@@ -1,23 +1,25 @@
-import nodemailer from 'nodemailer';
+// import nodemailer from 'nodemailer';
+import sgMail from '@sendgrid/mail';
 
 export default class Email {
   constructor(user, url) {
     this.to = user.email;
     this.firstName = user?.name?.split(' ')[0];
     this.url = url;
-    this.from = `Harshit Rathour <${process.env.EMAIL_FROM}>`;
+    this.from = process.env.SENDGRID_EMAIL_FROM;
+    // Set the SendGrid API key once in the constructor
+    sgMail.setApiKey(process.env.SENDGRID_APIKEY);
   }
-
   // Create transporter
-  transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: false, // If false, use TLS, otherwise SSL
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+  // transporter = nodemailer.createTransport({
+  //   host: process.env.EMAIL_HOST,
+  //   port: process.env.EMAIL_PORT,
+  //   secure: false, // If false, use TLS, otherwise SSL
+  //   auth: {
+  //     user: process.env.EMAIL_USER,
+  //     pass: process.env.EMAIL_PASSWORD,
+  //   },
+  // });
 
   // Send email method
   async send(subject) {
@@ -26,10 +28,22 @@ export default class Email {
       to: this.to,
       subject,
       text: `Hello ${this.firstName},\n\nPlease use the following link to reset your password: ${this.url}\n\nThe link is valid for 10 minutes.`,
+      html: `<p>Hello <strong>${this.firstName}</strong>,</p>
+             <p>Please use the following link to reset your password:</p>
+             <a href="${this.url}">${this.url}</a>
+             <p>The link is valid for 10 minutes.</p>`,
     };
-
-    // Send the email
-    await this.transporter.sendMail(mailOptions);
+    // // Send the email
+    // await this.transporter.sendMail(mailOptions);
+    try {
+      await sgMail.send(mailOptions);
+      console.log('✅ Email sent successfully');
+    } catch (error) {
+      console.error(
+        '❌ Error sending email:',
+        error.response?.body || error.message
+      );
+    }
   }
 
   // Password reset email method
